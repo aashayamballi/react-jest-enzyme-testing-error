73985d2aa508c506989a1a614c443f6550373939
Amballi <AA056649@Cerner.net>
2020-09-17 22:10:57 +0530

    first commit

---
+++ src/components/TestComponent1.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { HotTable } from "@handsontable/react";
+
+export default function TestComponent1() {
+  return <div>This is a test component 1</div>;
+}
---
+++ src/components/TestComponent2.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function TestComponent2() {
+  return <div>This is a test component 2</div>;
+}
